2d26dce3d4b64d563aec969203d4bfa372d24174
Sebastián González Montesinos <tagae@ehub.io>
2021-12-16 03:07:27 +0100

    feat(2021): add day 4 part 2 solution

--- day-4.clj
+++ day-4.clj
@@ -1,5 +1,6 @@
-(ns adventofcode.2021
-  (:use [clojure.string :only [split blank?]]))
+(ns adventofcode.2021.day-4
+  (:use [clojure.string :only [split blank?]]
+        clojure.test))
 
 (defn parse-draw-numbers
   [draw-line]
@@ -43,21 +44,53 @@
   (* called-number
      (/ (reduce (partial apply +) 0 winning-sets) 2)))
 
+(defn make-winner
+  [called-number winning-sets]
+  {:winning-sets winning-sets :last-number called-number})
+
+(defn find-winners
+  [draw-numbers boards]
+  (loop [[called-number & remaining-numbers] draw-numbers
+         candidates (map winning-sets boards)
+         found-winners '()]
+    (if called-number
+      (let [updated-candidates (map #(after-number-call called-number %) candidates)
+            grouped-candidates (group-by is-winner updated-candidates)
+            new-winners (map #(make-winner called-number %) (grouped-candidates true))
+            remaining-candidates (grouped-candidates nil)
+            updated-winners (concat found-winners new-winners)]
+        (recur remaining-numbers remaining-candidates updated-winners))
+      found-winners)))
+
+(defn find-winner
+  [pick lines]
+  (let [{:keys [draw-numbers boards]} (parse-lines lines)
+        {:keys [winning-sets last-number]} (pick (find-winners draw-numbers boards))]
+    (calculate-score last-number winning-sets)))
+
 (defn part1
   [lines]
-  (let [{:keys [draw-numbers boards]} (parse-lines lines)]
-    (loop [[called-number & remaining-numbers] draw-numbers
-           candidates (map winning-sets boards)]
-      (let [updated-candidates (map #(after-number-call called-number %) candidates)
-            winner (first (filter is-winner updated-candidates))]
-        (if winner
-          (calculate-score called-number winner)
-          (recur remaining-numbers updated-candidates))))))
+  (find-winner first lines))
+
+(defn part2
+  [lines]
+  (find-winner last lines))
 
 (defn with-line-seq
   [file-name func]
   (with-open [rdr (clojure.java.io/reader file-name)]
     (func (line-seq rdr))))
 
-(with-line-seq "day-4-example.txt" part1)
-(with-line-seq "day-4-input.txt" part1)
+(deftest part1-with-example
+  (is (= 4512 (with-line-seq "day-4-example.txt" part1))))
+
+(deftest part1-with-input
+  (is (= 22680 (with-line-seq "day-4-input.txt" part1))))
+
+(deftest part2-with-example
+  (is (= 1924 (with-line-seq "day-4-example.txt" part2))))
+
+(deftest part2-with-input
+  (is (= 16168 (with-line-seq "day-4-input.txt" part2))))
+
+(run-tests 'adventofcode.2021.day-4)
