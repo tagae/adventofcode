3887bc85aac5d1572ed65f52b14f96a4cb88eec1
Sebastián González Montesinos <tagae@ehub.io>
2021-12-27 12:14:47 -0500

    feat(2021): add day 9 part 2 solution

--- day-9.clj
+++ day-9.clj
@@ -1,32 +1,71 @@
 (ns adventofcode.2021.day-9
   (:use clojure.test))
 
+(defn heightmap-at
+  [heightmap x y]
+  (nth (nth heightmap y []) x ##Inf))
+
 (defn low-point?
   [heightmap x y]
-  (let [x-bound (dec (count (first heightmap)))
-        y-bound (dec (count heightmap))
-        height ((heightmap y) x)
-        left (if (> x 0) ((heightmap y) (dec x)) ##Inf)
-        right (if (< x x-bound) ((heightmap y) (inc x)) ##Inf)
-        up (if (> y 0) ((heightmap (dec y)) x) ##Inf)
-        down (if (< y y-bound) ((heightmap (inc y)) x) ##Inf)]
+  (let [height ((heightmap y) x)
+        left (heightmap-at heightmap (dec x) y)
+        right (heightmap-at heightmap (inc x) y)
+        up (heightmap-at heightmap x (dec y))
+        down (heightmap-at heightmap x (inc y))]
     (and (< height left)
          (< height right)
          (< height up)
          (< height down))))
 
-(defn risk-levels
-  [heightmap]
+(defn at-low-points
+  [heightmap func]
   (let [width (count (first heightmap))
         height (count heightmap)]
     (for [j (range 0 height)
           i (range 0 width)
           :when (low-point? heightmap i j)]
-      (inc ((heightmap j) i)))))
+      (func heightmap i j))))
+
+(defn risk-levels
+  [heightmap]
+  (at-low-points heightmap (fn [heightmap i j] (inc ((heightmap j) i)))))
 
 (defn part1
   [heightmap]
   (apply + (risk-levels heightmap)))
+
+(defn mark-at
+  [heightmap x y area]
+  (assoc heightmap y (assoc (heightmap y) x area)))
+
+(defn mark-basin-at
+  [heightmap x y]
+  (let [current (heightmap-at heightmap x y)]
+    (if (or (= current 9) (= current ##Inf) (symbol? current))
+      heightmap
+      (let [current (mark-at heightmap x y 'b)
+            up (mark-basin-at current x (inc y))
+            down (mark-basin-at up x (dec y))
+            right (mark-basin-at down (inc x) y)
+            left (mark-basin-at right (dec x) y)
+            result left]
+        result))))
+
+(defn basin-size
+  [heightmap]
+  (count (filter #{'b} (flatten heightmap))))
+
+(defn basin-size-at
+  [heightmap i j]
+  (basin-size (mark-basin-at heightmap i j)))
+
+(defn basin-sizes
+  [heightmap]
+  (at-low-points heightmap (fn [heightmap i j] (basin-size-at heightmap i j))))
+
+(defn part2
+  [heightmap]
+  (apply * (take 3 (sort > (basin-sizes heightmap)))))
 
 (defn parse-heightmap
   [lines]
@@ -51,5 +90,25 @@
 (deftest part-1-input
   (is (= 522 (with-line-seq "day-9-input.txt"
                (comp part1 parse-heightmap)))))
+
+(deftest part2-example-top-left
+  (is (= 3 (with-line-seq "day-9-example.txt"
+             (comp basin-size #(mark-basin-at % 0 0) parse-heightmap)))))
+
+(deftest part2-example-top-right
+  (is (= 9 (with-line-seq "day-9-example.txt"
+             (comp basin-size #(mark-basin-at % 9 0) parse-heightmap)))))
+
+(deftest part2-example-basin-sizes
+  (is (= '(3 9 14 9) (with-line-seq "day-9-example.txt"
+              (comp basin-sizes parse-heightmap)))))
+
+(deftest part2-example
+  (is (= 1134 (with-line-seq "day-9-example.txt"
+                (comp part2 parse-heightmap)))))
+
+(deftest part2-input
+  (is (= 916688 (with-line-seq "day-9-input.txt"
+                  (comp part2 parse-heightmap)))))
 
 (run-tests 'adventofcode.2021.day-9)
