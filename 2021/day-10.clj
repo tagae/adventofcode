4630ef13b11a863fac83a77bdad7a0111d5228aa
Sebastián González Montesinos <tagae@ehub.io>
2021-12-29 00:32:19 -0500

    feat(2021): add day 10 part 2 solution

--- 2021/day-10.clj
+++ 2021/day-10.clj
@@ -1,5 +1,6 @@
 (ns adventofcode.2021.day-10
-  (:use clojure.test))
+  (:use clojure.test
+        [clojure.string :only [join]]))
 
 (def matching-closings
   {\( \)
@@ -10,24 +11,20 @@
 (def valid-openings
   (set (keys matching-closings)))
 
-(defn first-illegal-closing
-  [line]
-  (loop [chunks line
-         openings '()]
-    (let [[head & tail] chunks
-          [last-opening & remaining-openings] openings]
+(defn check-chunks
+  "Returns the first non-matching closing character if any,
+   otherwise the list of openings that lack a closing."
+  [chunks]
+  (loop [[head & tail] chunks
+         pending-openings '()]
+    (if head
       (if (valid-openings head)
-        (recur tail (cons head openings))
-        (if (= head (matching-closings last-opening))
-          (recur tail remaining-openings)
-          head)))))
-
-(defn find-illegal-closings
-  [lines]
-  (for [line lines
-        :let [illegal-closing (first-illegal-closing line)]
-        :when illegal-closing]
-    illegal-closing))
+        (recur tail (cons head pending-openings))
+        (let [[last-opening & remaining-openings] pending-openings]
+          (if (= head (matching-closings last-opening))
+            (recur tail remaining-openings)
+            head)))
+      pending-openings)))
 
 (def illegal-closing-points
   {\) 3
@@ -35,9 +32,45 @@
    \} 1197
    \> 25137})
 
+(defn illegal-closing-chars
+  [lines]
+  (filter char? (map check-chunks lines)))
+
 (defn part1
   [lines]
-  (apply + (map illegal-closing-points (find-illegal-closings lines))))
+  (reduce + (map illegal-closing-points (illegal-closing-chars lines))))
+
+(defn closing-characters
+  [openings]
+  (map matching-closings openings))
+
+(def legal-closing-points
+  {\) 1
+   \] 2
+   \} 3
+   \> 4})
+
+(defn closing-characters-score
+  [closing-chars]
+  (reduce
+   (fn [total char] (+ (* total 5) (legal-closing-points char)))
+   0 closing-chars))
+
+(defn median
+  [scores]
+  (let [sorted (sort scores)
+        halfway (quot (count sorted) 2)]
+    (nth sorted halfway)))
+
+(defn pending-openings
+  [lines]
+  (filter seq? (map check-chunks lines)))
+
+(defn part2
+  [lines]
+  (median
+   (mapv (comp closing-characters-score closing-characters)
+         (pending-openings lines))))
 
 (defn with-line-seq
   [file-name func]
@@ -49,5 +82,15 @@
 
 (deftest part1-input
   (is (= 392043 (with-line-seq "day-10-input.txt" part1))))
+
+(deftest part2-example-5-closing
+  (is (= "])}>" (join (closing-characters
+                       (check-chunks "<{([{{}}[<[[[<>{}]]]>[]]"))))))
+
+(deftest part2-example
+  (is (= 288957 (with-line-seq "day-10-example.txt" part2))))
+
+(deftest part2-input
+  (is (= 1605968119 (with-line-seq "day-10-input.txt" part2))))
 
 (run-tests 'adventofcode.2021.day-10)
