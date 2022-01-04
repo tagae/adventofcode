5fac20a4dc0de545ecc0f0fd37c626b87efe0d52
Sebastián González Montesinos <tagae@ehub.io>
2022-01-04 00:05:30 -0500

    feat(2021): add day 13 part 2 solution

--- 2021/day-13.clj
+++ 2021/day-13.clj
@@ -13,16 +13,6 @@
        (let [[_ axis coord] (first (re-seq #"([xy])=(\d+)" line))]
          [(symbol axis) (Integer/parseInt coord 10)]))}))
 
-(defn shift-coords
-  [dots [dx dy]]
-  (map (fn [[x y]] [(+ x dx) (+ y dy)]) dots))
-
-(defn fold-delta
-  [axis coord]
-  (condp = axis
-    'x [(- coord) 0]
-    'y [0 (- coord)]))
-
 (defn abs
   [n]
   (if (< n 0) (- n) n))
@@ -46,6 +36,27 @@
   [{:keys [dots foldings]}]
   (fold-paper dots (take 1 foldings)))
 
+(defn dot-matrix
+  [dots]
+  (reduce (fn [result [x y]]
+            (if (< y (count result))
+              (let [row (result y)]
+                (assoc result y (conj (into row (repeat (- x (count row)) '.)) 'O)))
+              (conj result (conj (vec (repeat (dec x) '.)) 'O))))
+          []
+          (sort-by (fn [[x y]] [y x]) dots)))
+
+(defn print-matrix
+  [matrix]
+  (println)
+  (doseq [y (range 0 (count matrix))]
+    (println (matrix y)))
+  matrix)
+
+(defn part2
+  [{:keys [dots foldings]}]
+  (print-matrix (dot-matrix (fold-paper dots foldings))))
+
 (defn with-line-seq
   [file-name func]
   (with-open [rdr (clojure.java.io/reader file-name)]
@@ -63,5 +74,24 @@
 (deftest part1-example
   (is (= 621 (with-line-seq "day-13-input.txt"
                (comp count part1 parse-input)))))
+
+(deftest part2-example
+  (is (= '[[O O O O O]
+           [O . . . O]
+           [O . . . O]
+           [O . . . O]
+           [O O O O O]]
+         (with-line-seq "day-13-example.txt"
+           (comp part2 parse-input)))))
+
+(deftest part2-input
+  (is (= '[[O . . O . O . . O . O . . O . . . O O . . O O . . . O O . . . . O O . O O O O]
+           [O . . O . O . O . . O . . O . . . . O . O . . O . O . . O . . . . O . . . . O]
+           [O O O O . O O . . . O . . O . . . . O . O . . . . O . . O . . . . O . . . O]
+           [O . . O . O . O . . O . . O . . . . O . O . O O . O O O O . . . . O . . O]
+           [O . . O . O . O . . O . . O . O . . O . O . . O . O . . O . O . . O . O]
+           [O . . O . O . . O . . O O . . . O O . . . O O O . O . . O . . O O . . O O O O]]
+         (with-line-seq "day-13-input.txt"
+           (comp part2 parse-input)))))
 
 (run-tests 'adventofcode.2021.day-13)
